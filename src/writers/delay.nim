de11559e4d290ed3869d04b81e752bce458dad23
theduke <chris@theduke.at>
2015-11-24 03:14:03 +0100

    Added DelayWriter

---
+++ src/writers/delay.nim
@@ -0,0 +1,24 @@
+import os
+
+import ../nimlog
+
+type DelayWriter = ref object of Writer
+  # DelayWriter is only used for testing thread safety.
+  # It delays each write by delay ms.
+
+  delay: int
+  writer: Writer
+
+method doWrite*(w: DelayWriter, e: Entry) =
+  os.sleep(w.delay)
+  w.writer.write(e)
+
+method close*(w: DelayWriter, force: bool = false, wait: bool = true) =
+  w.writer.close(force, wait)
+
+proc newDelayWriter*(writer: Writer, delay: int = 100): DelayWriter =
+  DelayWriter(
+    `writer`: writer,
+    `delay`: delay,
+    minSeverity: Severity.CUSTOM
+  )
